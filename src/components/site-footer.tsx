ee9640d386815285fb01ee9f0b4d39c47bc392df
Aryan <engineer.aryan.work@gmail.com>
2025-08-15 17:50:06 +0000

    add here also

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { Logo } from './logo';
@@ -40,7 +39,7 @@ export function SiteFooter() {
             <h3 className="font-semibold mb-4 font-headline">{t('forArtisans')}</h3>
             <ul className="space-y-2">
               <li><Link href="/dashboard" className="text-sm hover:underline">{t('sellOnVirasat')}</Link></li>
-              <li><Link href="#" className="text-sm hover:underline">{t('artisanHandbook')}</Link></li>
+              <li><Link href="/dashboard/handbook" className="text-sm hover:underline">{t('artisanHandbook')}</Link></li>
             </ul>
           </div>
         </div>
