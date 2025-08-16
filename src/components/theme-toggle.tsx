2d0d8b7213f36ceb466ae62b2e0afc1d692d9138
Aryan <engineer.aryan.work@gmail.com>
2025-08-16 07:05:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -14,6 +15,21 @@ import {
 
 export function ThemeToggle() {
   const { setTheme } = useTheme()
+  const [mounted, setMounted] = React.useState(false)
+
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return (
+        <Button variant="ghost" size="icon" disabled>
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
+        </Button>
+    )
+  }
 
   return (
     <DropdownMenu>
