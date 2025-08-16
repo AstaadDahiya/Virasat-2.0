00e19a4d3272ba1cd218c8cfb2e6b424fd2766aa
Aryan <engineer.aryan.work@gmail.com>
2025-08-16 08:11:18 +0000

    add this in palceholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       <main className="flex-1">
         <section className="relative h-[60vh] w-full text-white">
           <Image
-            src="https://placehold.co/1200x500.png"
+            src="https://i.ibb.co/6g2Z3rQ/handcrafted-stories.jpg"
             alt="Artisan crafts"
             fill
             className="absolute z-0 object-cover"
